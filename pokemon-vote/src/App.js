51970445e467e221e377cf8120a8fc3d429ecfaa
likorn <likorn@ttu.ee>
2020-02-26 17:56:15 +0200

    welcome page design

--- pokemon-vote/src/App.js
+++ pokemon-vote/src/App.js
@@ -1,13 +1,79 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h2>Most popular Pokemon!</h2>
-      </div>
+      <nav
+        class="navbar navbar-expand-lg navbar-light fixed-top py-3"
+        id="mainNav"
+      >
+        <div class="container">
+          <button
+            class="navbar-toggler navbar-toggler-right"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarResponsive"
+            aria-controls="navbarResponsive"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto my-2 my-lg-0">
+              <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="#about">
+                  About
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="#services">
+                  Services
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="#portfolio">
+                  Portfolio
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="#contact">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+  render() {
+    return (
+      <header class="masthead">
+        <div class="container h-100">
+          <div class="row h-100 align-items-center justify-content-center text-center">
+            <div class="col-lg-10 align-self-end">
+              <h1 class="text-uppercase text-white font-weight-bold">
+                Vote for your favorite pokemon
+              </h1>
+              <hr class="divider my-4"></hr>
+            </div>
+            <div class="col-lg-8 align-self-baseline">
+              <p class="text-white-75 font-weight-light mb-5">
+                Pokemons are great, incredible creatures! They have rapidly
+                spread all over the world and became a vital part of life of
+                every teenager. Now we have decided to start a voting for the
+                best Pokemon ever! You decide!
+              </p>
+              <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">
+                Join voting
+              </a>
+            </div>
+          </div>
+        </div>
+      </header>
     );
   }
 }
